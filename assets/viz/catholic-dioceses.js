dcabc9d24b2e9e1aa3fdf8971d09a9fb3115b70e
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-04-07 21:36:47 -0400

    Cleanup the bar chart

--- assets/viz/catholic-dioceses.js
+++ assets/viz/catholic-dioceses.js
@@ -14,30 +14,30 @@ function getDecade(year) {
 class Visualization {
 
   constructor(id, data, dim, margin) {
-    this.data = data
-    this.margin = margin
+    this.data = data;
+    this.margin = margin;
 
     // Select the SVG, figure out the correct height, and use the
     // viewBox property to make it scale responsively.
-    this.svg = d3.select(id)
-    this.width = dim.width
-    this.height = dim.height
-    let outerWidth = this.width + this.margin.left + this.margin.right
-    let outerHeight = this.height + this.margin.top + this.margin.bottom
+    this.svg = d3.select(id);
+    this.width = dim.width;
+    this.height = dim.height;
+    const outerWidth = this.width + this.margin.left + this.margin.right;
+    const outerHeight = this.height + this.margin.top + this.margin.bottom;
     this.svg
-      .attr("viewBox", `0 0 ${outerWidth} ${outerHeight}`)
+      .attr("viewBox", `0 0 ${outerWidth} ${outerHeight}`);
     this.viz = this.svg
       .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`)
+      .attr("transform", `translate(${margin.left},${margin.top})`);
   }
 
 }
 
 class DiocesesMap extends Visualization {
   constructor(id, data, dim) {
-    const margin = { top: 10, right: 10, bottom: 10, left: 10 }
-    super(id, data, dim, margin)
-    this.year = d3.select("#year").node().valueAsNumber
+    const margin = { top: 10, right: 10, bottom: 10, left: 10 };
+    super(id, data, dim, margin);
+    this.year = d3.select("#year").node().valueAsNumber;
     this.projection = d3.geoAlbers()
       .translate([this.width / 2, this.height / 2 + 40])
       .scale(550);
@@ -51,7 +51,7 @@ class DiocesesMap extends Visualization {
       .text(this.year)
       .attr("y", 24)
       .attr("font-size", 48)
-      .attr("alignment-baseline", "top")
+      .attr("alignment-baseline", "top");
 
     this.viz
       .selectAll("path")
@@ -59,18 +59,18 @@ class DiocesesMap extends Visualization {
       .enter()
       .append("path")
       .attr("d", this.path)
-      .attr("class", "country")
+      .attr("class", "country");
 
     // Draw the stuff that gets updated
-    this.update(this.year)
+    this.update(this.year);
 
   }
 
   update(year) {
-    this.year = year
-    this.label.text(this.year)
+    this.year = year;
+    this.label.text(this.year);
 
-    let data = this.currentDioceses()
+    const data = this.currentDioceses();
 
     this.viz
       .selectAll("circle")
@@ -86,18 +86,18 @@ class DiocesesMap extends Visualization {
         } else {
           return "diocese";
         }
-      })
+      });
 
     this.viz
       .selectAll("circle")
       .data(data)
       .exit()
-      .remove()
+      .remove();
 
   }
 
   currentDioceses() {
-    return this.data.dioceses.filter(d => getYear(d.date_erected) <= this.year)
+    return this.data.dioceses.filter(d => getYear(d.date_erected) <= this.year);
   }
 
 }
@@ -105,36 +105,45 @@ class DiocesesMap extends Visualization {
 class DiocesesBarChart extends Visualization {
 
   constructor(id, data, dim) {
-    const margin = { top: 10, right: 40, bottom: 40, left: 10 }
-    super(id, data, dim, margin)
-    this.year = d3.select("#year").node().valueAsNumber
+    const margin = { top: 10, right: 40, bottom: 40, left: 10 };
+    super(id, data, dim, margin);
+    this.year = d3.select("#year").node().valueAsNumber;
     this.xScale = d3.scaleBand()
       .domain(d3.range(this.data.diocesesByDecade.length))
       .range([0, this.width])
-      .padding(0.1)
+      .padding(0.1);
     this.xAxis = d3.axisBottom()
       .scale(this.xScale)
-      .tickValues(this.xScale.domain().filter((d, i) => !((i + 1) % 10)))
-      .tickFormat(i => this.data.diocesesByDecade[i].decade)
+      .tickValues(this.xScale.domain().filter((d, i) => !((i + 1) % 5)))
+      .tickFormat(i => this.data.diocesesByDecade[i].decade);
     this.yScale = d3.scaleLinear(
       [0, d3.max(this.data.diocesesByDecade, d => d.count)],
-      [this.height, 0])
+      [this.height, 0]);
     this.yAxis = d3.axisRight()
       .scale(this.yScale)
       .ticks(10);
   }
 
+  // Draw the unchanging parts of the visualization
   render() {
+
+    this.viz
+      .append("text")
+      .attr("x", 0)
+      .attr("y", 30)
+      .text("Dioceses erected per decade");
+
     this.viz
       .append("g")
       .attr("class", "x axis")
-      .attr("transform", `translate(0,${this.height})`)
-      .call(this.xAxis)
+      .attr("transform", `translate(0,${this.height + 1})`)
+      .call(this.xAxis);
+
     this.viz
       .append("g")
       .attr("class", "y axis")
-      .attr("transform", `translate(${this.width},0)`)
-      .call(this.yAxis)
+      .attr("transform", `translate(${this.width + 1},0)`)
+      .call(this.yAxis);
 
     this.viz
       .selectAll("rect")
@@ -144,12 +153,13 @@ class DiocesesBarChart extends Visualization {
       .attr("x", (d, i) => this.xScale(i))
       .attr("y", d => this.yScale(d.count))
       .attr("width", this.xScale.bandwidth())
-      .attr("height", d => this.yScale(0) - this.yScale(d.count))
+      .attr("height", d => this.yScale(0) - this.yScale(d.count));
 
     // Draw the stuff that gets updated
-    this.update(this.year)
+    this.update(this.year);
   }
 
+  // Draw the changing parts of the visualization
   update(year) {
     this.viz
       .selectAll("rect")
@@ -158,16 +168,17 @@ class DiocesesBarChart extends Visualization {
       .filter(d => d.decade === getDecade(year))
       .classed("active", true);
   }
+
 }
 
 // Begin actually loading the data and drawing the visualizations
-const urls = [
+let urls = [
   "/visualizations/catholic-dioceses/catholic-dioceses.json",
   "/visualizations/catholic-dioceses/dioceses-by-decade.json",
   "/visualizations/catholic-dioceses/north-america.json"
-]
-let promises = []
-urls.forEach(url => promises.push(d3.json(url)))
+];
+const promises = [];
+urls.forEach(url => promises.push(d3.json(url)));
 
 // Once all the data is loaded, initialize and render the visualizations
 Promise.all(promises)
@@ -177,20 +188,20 @@ Promise.all(promises)
       "#map",
       { dioceses: data[0], northamerica: data[2] },
       { width: 1000, height: 600 }
-    )
+    );
     const chart = new DiocesesBarChart(
       "#barchart",
       { diocesesByDecade: data[1] },
       { width: 400, height: 200 }
-    )
-    map.render()
-    chart.render()
+    );
+    map.render();
+    chart.render();
 
     // Listen for changes to the slider
     d3.select("#year").on("input", function () {
-      let year = this.valueAsNumber
+      const year = this.valueAsNumber;
       map.update(year);
-      chart.update(year)
+      chart.update(year);
     });
 
   });
