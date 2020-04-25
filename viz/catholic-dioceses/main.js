2e10c75b09b8e0e0f0100b08470545d2b014e83b
Lincoln Mullen <lincoln@lincolnmullen.com>
2020-04-25 14:46:26 -0400

    Accept easily made eslint changes

--- viz/catholic-dioceses/main.js
+++ viz/catholic-dioceses/main.js
@@ -1,9 +1,9 @@
-import * as d3 from "d3";
+import * as d3 from 'd3';
 import './style.css';
 
 // Take in an ISO-8601 string and return just the year as an integer
 function getYear(dateString) {
-  const year = dateString.split("-");
+  const year = dateString.split('-');
   return parseInt(year, 10);
 }
 
@@ -13,7 +13,6 @@ function getDecade(year) {
 }
 
 class Visualization {
-
   constructor(id, data, dim, margin) {
     this.data = data;
     this.margin = margin;
@@ -25,24 +24,24 @@ class Visualization {
     this.height = dim.height;
     const outerWidth = this.width + this.margin.left + this.margin.right;
     const outerHeight = this.height + this.margin.top + this.margin.bottom;
-    this.svg.attr("viewBox", `0 0 ${outerWidth} ${outerHeight}`);
+    this.svg.attr('viewBox', `0 0 ${outerWidth} ${outerHeight}`);
 
     // The viz is the usable part of the plot, excluding the margins
     this.viz = this.svg
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
   }
-
 }
 
 class DiocesesMap extends Visualization {
-
   constructor(id, data, dim) {
-    const margin = { top: 10, right: 10, bottom: 10, left: 10 };
+    const margin = {
+      top: 10, right: 10, bottom: 10, left: 10,
+    };
 
     super(id, data, dim, margin);
 
-    this.year = d3.select("#year").node().valueAsNumber;
+    this.year = d3.select('#year').node().valueAsNumber;
     this.projection = d3.geoAlbers()
       .translate([this.width / 2 + 40, this.height / 2 + 40])
       .scale(500);
@@ -53,51 +52,50 @@ class DiocesesMap extends Visualization {
   render() {
     // Label for the year
     this.label = this.viz
-      .append("text")
+      .append('text')
       .text(this.year)
-      .attr("y", 24)
-      .attr("font-size", 48)
-      .attr("alignment-baseline", "top");
+      .attr('y', 24)
+      .attr('font-size', 48)
+      .attr('alignment-baseline', 'top');
 
     // Legend for the types of dioceses
     const legend = this.viz
-      .append("g")
-      .attr("transform", "translate(8,50)");
-    legend.append("circle")
-      .attr("cx", 0)
-      .attr("cy", 0)
-      .attr("r", 3)
-      .attr("class", "diocese")
-      .classed("legend", true);
+      .append('g')
+      .attr('transform', 'translate(8,50)');
+    legend.append('circle')
+      .attr('cx', 0)
+      .attr('cy', 0)
+      .attr('r', 3)
+      .attr('class', 'diocese')
+      .classed('legend', true);
     legend
-      .append("text")
-      .attr("x", 10)
-      .attr("y", 5)
-      .text("Diocese");
+      .append('text')
+      .attr('x', 10)
+      .attr('y', 5)
+      .text('Diocese');
     legend
-      .append("circle")
-      .attr("cx", 0)
-      .attr("cy", 25)
-      .attr("r", 3)
-      .attr("class", "metropolitan")
-      .classed("legend", true);
+      .append('circle')
+      .attr('cx', 0)
+      .attr('cy', 25)
+      .attr('r', 3)
+      .attr('class', 'metropolitan')
+      .classed('legend', true);
     legend
-      .append("text")
-      .attr("x", 10)
-      .attr("y", 30)
-      .text("Archdiocese");
+      .append('text')
+      .attr('x', 10)
+      .attr('y', 30)
+      .text('Archdiocese');
 
     this.viz
-      .selectAll("path")
+      .selectAll('path')
       .data(this.data.northamerica.features)
       .enter()
-      .append("path")
-      .attr("d", this.path)
-      .attr("class", "country");
+      .append('path')
+      .attr('d', this.path)
+      .attr('class', 'country');
 
     // On first render, draw the stuff that gets updated
     this.update(this.year);
-
   }
 
   // Draw the stuff that gets updated
@@ -106,60 +104,54 @@ class DiocesesMap extends Visualization {
     this.label.text(this.year);
 
     this.viz
-      .selectAll("circle:not(.legend)")
+      .selectAll('circle:not(.legend)')
       .data(this.currentDioceses())
       .join(
-        enter => enter
-          .append("circle")
-          .attr("cx", d => this.projection([d.lon, d.lat])[0])
-          .attr("cy", d => this.projection([d.lon, d.lat])[1])
-          .attr("r", "3px")
-          .attr("class", d => this.dioceseType(d.date_metropolitan, year)),
-        update => update
-          .attr("class", d => this.dioceseType(d.date_metropolitan, year)),
-        exit => exit
-          .remove()
+        (enter) => enter
+          .append('circle')
+          .attr('cx', (d) => this.projection([d.lon, d.lat])[0])
+          .attr('cy', (d) => this.projection([d.lon, d.lat])[1])
+          .attr('r', '3px')
+          .attr('class', (d) => this.dioceseType(d.date_metropolitan, year)),
+        (update) => update
+          .attr('class', (d) => this.dioceseType(d.date_metropolitan, year)),
+        (exit) => exit
+          .remove(),
       );
 
-    const tooltip = d3.select("body").append("div")
-      .attr("class", "tooltip")
-      .style("position", "absolute")
-      .style("visibility", "hidden");
+    const tooltip = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('position', 'absolute')
+      .style('visibility', 'hidden');
 
     this.viz
-      .selectAll("circle")
-      .on("mouseover", function (d) {
-        const text =
-          `Diocese of ${d.city} in ${d.state}<br/>` +
-          `Founded ${getYear(d.date_erected)}`;
+      .selectAll('circle')
+      .on('mouseover', (d) => {
+        const text = `Diocese of ${d.city} in ${d.state}<br/>`
+          + `Founded ${getYear(d.date_erected)}`;
         tooltip.html(text);
-        tooltip.style("visibility", "visible");
+        tooltip.style('visibility', 'visible');
       })
-      .on("mousemove", function () {
-        return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
-      })
-      .on("mouseout", function () {
-        return tooltip.style("visibility", "hidden");
-      });
-
+      .on('mousemove', () => tooltip.style('top', `${d3.event.pageY - 10}px`).style('left', `${d3.event.pageX + 10}px`))
+      .on('mouseout', () => tooltip.style('visibility', 'hidden'));
   }
 
   // Filter the data down to the dioceses that should be displayed in a year
   currentDioceses() {
-    return this.data.dioceses.filter(d => getYear(d.date_erected) <= this.year);
+    return this.data.dioceses.filter((d) => getYear(d.date_erected) <= this.year);
   }
 
   // Return the class for the type of diocese
   dioceseType(dateMetropolitan, year) {
-    return getYear(dateMetropolitan) <= year ? "metropolitan" : "diocese";
+    return getYear(dateMetropolitan) <= year ? 'metropolitan' : 'diocese';
   }
-
 }
 
 class DiocesesRiteMap extends Visualization {
-
   constructor(id, data, dim) {
-    const margin = { top: 10, right: 10, bottom: 10, left: 10 };
+    const margin = {
+      top: 10, right: 10, bottom: 10, left: 10,
+    };
     super(id, data, dim, margin);
     this.colorScale = d3.scaleOrdinal(d3.schemeSet1);
     this.projection = d3.geoAlbers()
@@ -170,77 +162,72 @@ class DiocesesRiteMap extends Visualization {
 
   render() {
     this.viz
-      .selectAll("path")
+      .selectAll('path')
       .data(this.data.northamerica.features)
       .enter()
-      .append("path")
-      .attr("d", this.path)
-      .attr("class", "country");
+      .append('path')
+      .attr('d', this.path)
+      .attr('class', 'country');
 
     this.viz
-      .selectAll("circle:not(.legend)")
+      .selectAll('circle:not(.legend)')
       .data(this.currentDioceses())
-      .join("circle")
-      .attr("cx", d => this.projection([d.lon, d.lat])[0])
-      .attr("cy", d => this.projection([d.lon, d.lat])[1])
-      .attr("r", 5)
-      .style("fill", d => this.colorScale(d.rite))
-      .classed("nonlatin", true);
+      .join('circle')
+      .attr('cx', (d) => this.projection([d.lon, d.lat])[0])
+      .attr('cy', (d) => this.projection([d.lon, d.lat])[1])
+      .attr('r', 5)
+      .style('fill', (d) => this.colorScale(d.rite))
+      .classed('nonlatin', true);
 
     const legend = this.viz
-      .append("g")
-      .attr("transform", "translate(0,10)");
+      .append('g')
+      .attr('transform', 'translate(0,10)');
 
     const rites = this.colorScale.domain();
-    for (let i = 0; i < rites.length; i++) {
-      legend.append("circle")
-        .attr("cx", 0)
-        .attr("cy", 25 * i)
-        .attr("r", 5)
-        .style("fill", this.colorScale(rites[i]))
-        .classed("nonlatin", true)
-        .classed("legend", true);
+    for (let i = 0; i < rites.length; i += 1) {
+      legend.append('circle')
+        .attr('cx', 0)
+        .attr('cy', 25 * i)
+        .attr('r', 5)
+        .style('fill', this.colorScale(rites[i]))
+        .classed('nonlatin', true)
+        .classed('legend', true);
       legend
-        .append("text")
-        .attr("x", 10)
-        .attr("y", 5 + 25 * i)
-        .text(rites[i] + " Rite");
+        .append('text')
+        .attr('x', 10)
+        .attr('y', 5 + 25 * i)
+        .text(`${rites[i]} Rite`);
     }
 
-    const tooltip = d3.select("body").append("div")
-      .attr("class", "tooltip")
-      .style("position", "absolute")
-      .style("visibility", "hidden")
+    const tooltip = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('position', 'absolute')
+      .style('visibility', 'hidden')
       .text("I'm a circle!");
 
     this.viz
-      .selectAll("circle")
-      .on("mouseover", function (d) {
+      .selectAll('circle')
+      .on('mouseover', (d) => {
         const text = `Diocese of ${d.city} in ${d.state}<br/>${d.rite} Rite`;
         tooltip.html(text);
-        tooltip.style("visibility", "visible");
+        tooltip.style('visibility', 'visible');
       })
-      .on("mousemove", function () {
-        return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
-      })
-      .on("mouseout", function () {
-        return tooltip.style("visibility", "hidden");
-      });
-
+      .on('mousemove', () => tooltip.style('top', `${d3.event.pageY - 10}px`).style('left', `${d3.event.pageX + 10}px`))
+      .on('mouseout', () => tooltip.style('visibility', 'hidden'));
   }
 
   currentDioceses() {
-    return this.data.dioceses.filter(d => d.rite != "Latin");
+    return this.data.dioceses.filter((d) => d.rite !== 'Latin');
   }
-
 }
 
 class DiocesesBarChart extends Visualization {
-
   constructor(id, data, dim) {
-    const margin = { top: 10, right: 40, bottom: 40, left: 10 };
+    const margin = {
+      top: 10, right: 40, bottom: 40, left: 10,
+    };
     super(id, data, dim, margin);
-    this.year = d3.select("#year").node().valueAsNumber;
+    this.year = d3.select('#year').node().valueAsNumber;
     this.xScale = d3.scaleBand()
       .domain(d3.range(this.data.diocesesByDecade.length))
       .range([0, this.width])
@@ -248,10 +235,11 @@ class DiocesesBarChart extends Visualization {
     this.xAxis = d3.axisBottom()
       .scale(this.xScale)
       .tickValues(this.xScale.domain().filter((d, i) => !((i + 1) % 5)))
-      .tickFormat(i => this.data.diocesesByDecade[i].decade);
+      .tickFormat((i) => this.data.diocesesByDecade[i].decade);
     this.yScale = d3.scaleLinear(
-      [0, d3.max(this.data.diocesesByDecade, d => d.count)],
-      [this.height, 0]);
+      [0, d3.max(this.data.diocesesByDecade, (d) => d.count)],
+      [this.height, 0],
+    );
     this.yAxis = d3.axisRight()
       .scale(this.yScale)
       .ticks(10);
@@ -259,34 +247,33 @@ class DiocesesBarChart extends Visualization {
 
   // Draw the unchanging parts of the visualization
   render() {
+    this.viz
+      .append('text')
+      .attr('x', 0)
+      .attr('y', 30)
+      .text('Dioceses erected per decade');
 
     this.viz
-      .append("text")
-      .attr("x", 0)
-      .attr("y", 30)
-      .text("Dioceses erected per decade");
-
-    this.viz
-      .append("g")
-      .attr("class", "x axis")
-      .attr("transform", `translate(0,${this.height + 1})`)
+      .append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0,${this.height + 1})`)
       .call(this.xAxis);
 
     this.viz
-      .append("g")
-      .attr("class", "y axis")
-      .attr("transform", `translate(${this.width + 1},0)`)
+      .append('g')
+      .attr('class', 'y axis')
+      .attr('transform', `translate(${this.width + 1},0)`)
       .call(this.yAxis);
 
     this.viz
-      .selectAll("rect")
+      .selectAll('rect')
       .data(this.data.diocesesByDecade)
       .enter()
-      .append("rect")
-      .attr("x", (d, i) => this.xScale(i))
-      .attr("y", d => this.yScale(d.count))
-      .attr("width", this.xScale.bandwidth())
-      .attr("height", d => this.yScale(0) - this.yScale(d.count));
+      .append('rect')
+      .attr('x', (d, i) => this.xScale(i))
+      .attr('y', (d) => this.yScale(d.count))
+      .attr('width', this.xScale.bandwidth())
+      .attr('height', (d) => this.yScale(0) - this.yScale(d.count));
 
     // On first render, draw the stuff that gets updated
     this.update(this.year);
@@ -295,55 +282,52 @@ class DiocesesBarChart extends Visualization {
   // Draw the changing parts of the visualization
   update(year) {
     this.viz
-      .selectAll("rect")
+      .selectAll('rect')
       .data(this.data.diocesesByDecade)
-      .classed("active", false)
-      .filter(d => d.decade === getDecade(year))
-      .classed("active", true);
+      .classed('active', false)
+      .filter((d) => d.decade === getDecade(year))
+      .classed('active', true);
   }
-
 }
 
 // Begin actually loading the data and drawing the visualizations
-let urls = [
-  "/visualizations/catholic-dioceses/catholic-dioceses.json",
-  "/visualizations/catholic-dioceses/dioceses-by-decade.json",
-  "/visualizations/catholic-dioceses/north-america.json"
+const urls = [
+  '/visualizations/catholic-dioceses/catholic-dioceses.json',
+  '/visualizations/catholic-dioceses/dioceses-by-decade.json',
+  '/visualizations/catholic-dioceses/north-america.json',
 ];
 const promises = [];
-urls.forEach(url => promises.push(d3.json(url)));
+urls.forEach((url) => promises.push(d3.json(url)));
 
 // Once all the data is loaded, initialize and render the visualizations
 Promise.all(promises)
-  .then(function (data) {
-
+  .then((data) => {
     const chronoMap = new DiocesesMap(
-      "#chrono-map",
+      '#chrono-map',
       { dioceses: data[0], northamerica: data[2] },
-      { width: 1000, height: 600 }
+      { width: 1000, height: 600 },
     );
     chronoMap.render();
 
     const chart = new DiocesesBarChart(
-      "#barchart",
+      '#barchart',
       { diocesesByDecade: data[1] },
-      { width: 400, height: 200 }
+      { width: 400, height: 200 },
     );
     chart.render();
 
     const riteMap = new DiocesesRiteMap(
-      "#rite-map",
+      '#rite-map',
       { dioceses: data[0], northamerica: data[2] },
-      { width: 1000, height: 600 }
+      { width: 1000, height: 600 },
     );
     riteMap.render();
 
 
     // Listen for changes to the slider
-    d3.select("#year").on("input", function () {
+    d3.select('#year').on('input', function () {
       const year = this.valueAsNumber;
       chronoMap.update(year);
       chart.update(year);
     });
-
   });
