ebabf238c1483dabbaf5d59fc07092bce1c56387
Kim Nguyen <design@kimisgold.com>
2020-12-10 16:14:29 -0500

    Add skip link. #32

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+$(document).ready(function() {
+    $('.show-on-focus').keydown(function(e) {
+      if (e.keyCode == 13) {
+        $('#content').focus();
+      }
+    });
+});
