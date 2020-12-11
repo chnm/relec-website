47d60fa23de81985afa9ad68f37e1e6de9e3f816
Kim Nguyen <design@kimisgold.com>
2020-12-11 13:13:09 -0500

    Only give #content tabindex when receiving skip nav focus. #32

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,11 @@ $(document).foundation();
 $(document).ready(function() {
     $('.show-on-focus').keydown(function(e) {
       if (e.keyCode == 13) {
-        $('#content').focus();
+        $('#content').attr('tabindex', '-1').focus();
       }
+    });
+    
+    $('#content').on('blur focusout', function() {
+        $(this).removeAttr('tabindex');
     });
 });
